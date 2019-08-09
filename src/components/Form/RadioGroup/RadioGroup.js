d3c0c7f4f5ff50a0ca25d6289caa1f11e0b97879
ajt79 <andrew.thyng@txstate.edui>
2019-08-09 13:51:16 -0500

    Fixed accessibility on radio group

--- src/components/Form/RadioGroup/RadioGroup.js
+++ src/components/Form/RadioGroup/RadioGroup.js
@@ -4,7 +4,13 @@ import { Stack } from '../../Stack'
 import { Form } from '../Form'
 import styled from 'styled-components'
 
-const Label = styled.span``
+const Label = styled.legend``
+
+const FieldSet = styled.fieldset`
+  border: none;
+  margin: 0;
+  padding: 0;
+`
 
 export const RadioGroup = props => {
   const { children, label, stackProps, path, id } = props
@@ -21,9 +27,11 @@ export const RadioGroup = props => {
       id={id}
       onChange={onChange}
     >
-      <Label>{label}</Label>
       <Stack {...stackProps}>
-        {children}
+        <FieldSet role='group' >
+          <Label>{label}</Label>
+          {children}
+        </FieldSet>
       </Stack>
     </Form>
   )
