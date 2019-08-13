3228a717fd1f212ed04476d467be1f1396d9f951
ajt79 <andrew.thyng@txstate.edui>
2019-08-13 15:12:25 -0500

    Fixed spacing and unused elements in ChoiceGroup

--- src/components/Choicegroup/Choicegroup.js
+++ src/components/Choicegroup/Choicegroup.js
@@ -1,31 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { ChoiceGroup, IChoiceGroupOption } from 'office-ui-fabric-react/lib/ChoiceGroup';
-import { Label } from 'office-ui-fabric-react/lib/Label';
-import { getId } from 'office-ui-fabric-react/lib/Utilities';
-
-//const Label = styled.label``
+import { ChoiceGroup } from 'office-ui-fabric-react/lib/ChoiceGroup'
+import { Label } from 'office-ui-fabric-react/lib/Label'
 
 export const Choicegroup = props => {
-    const { name, label, options, ariaLabel } = props
+  const { label, options, ariaLabel } = props
 
-    return (
-        <>
-            <Label id='radioElement' htmlFor='choicegroup'>{label}</Label>
-            <ChoiceGroup
-                id='choicegroup'
-                role='radiogroup'
-                ariaLabel={ariaLabel}
-                ariaLabelledBy='radioElement'
-                defaultSelectedKey='A'
-                options={options}
-                required={true}
-            ></ChoiceGroup>
-        </>
-    )
+  return (
+    <>
+      <Label id='radioElement' htmlFor='choicegroup'>{label}</Label>
+      <ChoiceGroup
+        id='choicegroup'
+        role='radiogroup'
+        ariaLabel={ariaLabel}
+        ariaLabelledBy='radioElement'
+        defaultSelectedKey='A'
+        options={options}
+        required
+      />
+    </>
+  )
 }
+
 Choicegroup.propTypes = {
-    ariaLabel: PropTypes.string.isRequired
+  ariaLabel: PropTypes.string.isRequired
 }
-
--- src/stories/Choicegroup.stories.jsx
+++ src/stories/Choicegroup.stories.jsx
@@ -3,32 +3,31 @@ import { storiesOf } from '@storybook/react'
 import { Choicegroup } from '../components'
 
 storiesOf('Choicegroup', module)
-    .add('basic', () => {
-        return <Choicegroup
-            ariaLabel='Choicegroup1'
-            label='Select One: '
-            required= 'true'
-            name='Test'
-            options={
-                [
-                    {
-                        key:'A',
-                        text:'Apple'
-                    },
-                    {
-                        key:'B',
-                        text:'Banana'
-                    },
-                    {
-                        key:'C',
-                        text:'Carrot'
-                    },
-                    {
-                        key:'D',
-                        text:'Dragon Fruit'
-                    }
-                ]
-            }
-        >
-        </Choicegroup>
-    })
+  .add('basic', () => {
+    return <Choicegroup
+      ariaLabel='Choicegroup1'
+      label='Select One: '
+      required='true'
+      name='Test'
+      options={
+        [
+          {
+            key: 'A',
+            text: 'Apple'
+          },
+          {
+            key: 'B',
+            text: 'Banana'
+          },
+          {
+            key: 'C',
+            text: 'Carrot'
+          },
+          {
+            key: 'D',
+            text: 'Dragon Fruit'
+          }
+        ]
+      }
+    />
+  })
