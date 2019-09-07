c092ff5e7b1a826445d4daeca82f89081c0391da
Andrew Thyng <andrew.thyng@gmail.com>
2019-09-07 14:02:21 -0500

    Testing out an email template editor

--- src/components/EmailTemplate/EmailTemplate.js
+++ src/components/EmailTemplate/EmailTemplate.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import EmailEditor from 'react-email-editor'
 
 export const EmailTemplate = () => {
-  return <div />
+  return (
+    <div style={{ position: 'absolute', top: 0, bottom: 0, width: '100%' }}>
+      <EmailEditor minHeight='100%' />)
+    </div>
+  )
 }
