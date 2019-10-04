2adc128eb8c4141160a39abdaa041c7390c5dafe
kevinpena <kep85@txstate.edu>
2019-10-04 14:11:30 -0500

    adding native props

--- src/components/Button/index.d.ts
+++ src/components/Button/index.d.ts
@@ -1,11 +1,14 @@
 import * as React from 'react';
 
 export type ButtonVariant = "primary" | "outline" | "transparent";
+export type ButtonType = "submit" | "button" | "reset";
 
 export interface ButtonProps {
   label: string;
-  ariaLabel: string;
+  ariaLabel?: string;
   variant?: ButtonVariant;
+  disabled?: boolean;
+  type?: ButtonType;
   onClick?: (...args: any[]) => any;
 }
 
