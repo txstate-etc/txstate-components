c5a3859bccc9f2708629b6f1f85ec22ff140b6d4
Nick Wing <nwing@txstate.edu>
2019-10-28 09:07:33 -0500

    add dependency to useEffect in the useSubject hook

--- src/hooks/useSubject.js
+++ src/hooks/useSubject.js
@@ -5,7 +5,7 @@ export const useSubject = subject => {
   useEffect(() => {
     const sub = subject.pipe(skip(1)).subscribe(s => setState(s))
     return () => sub.unsubscribe()
-  })
+  }, [subject])
   const newSetState = state => subject.next(state)
   return [value, newSetState]
 }
