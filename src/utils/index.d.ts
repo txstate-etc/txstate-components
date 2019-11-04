9ba8d10e64f16a4f955cbf337e7e3dcd4a23ff40
Nick Wing <nwing@txstate.edu>
2019-11-03 22:00:05 -0600

    Updated types related to useSubject

--- src/utils/index.d.ts
+++ src/utils/index.d.ts
@@ -14,7 +14,8 @@ export interface ISubject {
 export const Subject: ISubject
 
 export type DerivedSubjectTransform = <InputType, OutputType>(input:InputType) => OutputType
+export type DerivedSubjectMutate = <InputType, OutputType>(value:OutputType, parentValue:InputType) => InputType
 export interface IDerivedSubject<OutputType = any, InputType = any> extends BehaviorSubject<InputType> {
-  new (subject:UsableSubject, transform:DerivedSubjectTransform<InputType, OutputType>): IDerivedSubject<OutputType, InputType>
+  new (subject:UsableSubject<OutputType>, transform:DerivedSubjectTransform<InputType, OutputType>, mutate?: DerivedSubjectMutate<InputType, OutputType>): IDerivedSubject<OutputType, InputType>
 }
 export const DerivedSubject:IDerivedSubject
