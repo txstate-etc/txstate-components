01427a47c13bf9216b1ab42b4d6b891a79ff96e9
Ross McEwen <ross@txstate.edu>
2019-11-15 15:45:43 -0600

    Export TagItem from typescript declaration

--- src/components/TagItem/index.d.ts
+++ src/components/TagItem/index.d.ts
@@ -1,2 +1,4 @@
 import * as React from 'react'
 export { ITag, ITagItemProps, ITagItemStyles, ITagItemStyleProps } from 'office-ui-fabric-react/lib/components/pickers/TagPicker/TagPicker.types'
+
+export const TagItem: React.FC<TagItemProps>
