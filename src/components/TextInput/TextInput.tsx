5ba4cc0cf395be4fca0e98d869e914f06d94aa70
Andrew Thyng <andrew@thyng.dev>
2020-03-04 21:06:30 -0600

    Skeleton for text input

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,19 +1,72 @@
+/** @jsx jsx */
 import React, { useRef } from 'react'
+import { css, jsx } from '@emotion/core'
 import nanoid from 'nanoid'
-import { TextInputComponent } from './TextInput.types'
 
-export const TextInput: TextInputComponent = props => {
-  const { id, label } = props
+const Description = (props: { text?: string }) => {
+  if (!props.text) {
+    return null
+  }
+
+  return (
+    <span css={css`
+      margin: 6px 0 0 0;
+      font-family: sans-serif;
+      font-size: 0.9rem;
+    `}>
+      {props.text}
+    </span>
+  )
+}
+
+export interface TextInputProps {
+  label: string
+  description?: string
+  placeholder?: string
+  style?: React.CSSProperties
+  id?: string
+}
+
+export const TextInput: React.FunctionComponent<TextInputProps> = props => {
+  const { id, style, placeholder, label, description } = props
 
   const _id = useRef(id ?? nanoid(10))
 
   return (
-    <div>
-      <label htmlFor={_id.current}>{label}</label>
+    <div
+      style={style}
+      css={css`
+        display: flex;
+        flex-direction: column;
+        font-family: sans-serif;
+      `}
+    >
+      <label
+        css={css`
+          margin: 0 0 6px 0;
+          font-size: 100%;
+        `}
+        htmlFor={_id.current}
+      >
+        {label}
+      </label>
       <input
+        css={css`
+          background-color: white;
+          border: solid 1px #DADADA;
+          padding: 4px;
+          border-radius: 3px;
+
+          &:focus {
+            outline: none;
+            box-shadow: 0 0 5px -3px blue;
+          }
+        `}
         id={_id.current}
+        placeholder={placeholder}
         type='text'
       />
+      <Description text={description} />
     </div>
   )
 }
--- src/components/TextInput/TextInput.types.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-export interface TextInputProps {
-  label: string
-  id?: string
-}
-
-export type TextInputComponent = React.FunctionComponent<TextInputProps>
