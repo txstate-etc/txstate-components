33d568ee3542ed9bbc15b2ea132cfc65eea4907a
ajt79 <ajt79@txstate.edu>
2020-03-11 15:08:36 -0500

    Made a11y addon work properly

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,5 @@
-import { configure } from '@storybook/react'
+import { configure, addDecorator } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
 
+addDecorator(withA11y)
 configure(require.context('../src/stories', true, /stories.[mtj][ds]x?/), module)
---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface CheckboxProps {
+}
+
+type Checkbox = React.FunctionComponent<CheckboxProps>
+
+export const Checkbox: Checkbox = props => {
+  return (
+    <div />
+  )
+}
