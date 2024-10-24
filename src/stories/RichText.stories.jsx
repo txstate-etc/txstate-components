b56cc77bdfa67e4513ca22d00be855789c71ff2d
kevinpena <kep85@txstate.edu>
2024-10-24 12:33:50 -0500

    adding rich text story

---
+++ src/stories/RichText.stories.jsx
@@ -0,0 +1,11 @@
+import { RichText } from "../components";
+
+export default {
+  title: 'Components | Rich Text',
+  component: RichText
+}
+
+export const RichTextStory = {
+  name: 'Rich Text',
+  component: RichText
+}
